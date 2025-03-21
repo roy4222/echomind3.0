0993581536bf0638440c72917be58b67d08d2ca5
roy422 <roy422roy@gmail.com>
2025-03-21 11:27:32 +0800

    優化環境變數檢查邏輯，將過濾函數的參數解構方式進行調整，以提升代碼可讀性。

--- src/lib/utils/upload.ts
+++ src/lib/utils/upload.ts
@@ -18,7 +18,7 @@ const checkEnvVariables = () => {
   };
 
   const missingVars = Object.entries(requiredEnvVars)
-    .filter(([_, value]) => !value)
+    .filter(([, value]) => !value)
     .map(([key]) => key);
 
   if (missingVars.length > 0) {
