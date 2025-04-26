44a0b4760b5a89f2b685cc09f9d45de95b77b0da
GuaGua0216 <171306165+GuaGua0216@users.noreply.github.com>
2025-04-26 16:03:32 +0800

    拼圖合併

---
+++ frontend/app/game/puzzle/app/constants/puzzleThemes.ts
@@ -0,0 +1,32 @@
+import { PuzzleTheme } from '../types';
+
+export const puzzleThemes: PuzzleTheme[] = [
+  {
+    id: "1",
+    title: "re0 愛蜜莉雅",
+    image: "/images/1.png",
+    description: "弧菌之",
+    placeholder: "/globe.svg",
+  },
+  {
+    id: "2",
+    title: "間諜家家酒",
+    image: "/images/4.png", 
+    description: "各種可愛動物的拼圖",
+    placeholder: "/globe.svg",
+  },
+  {
+    id: "3",
+    title: "敗北女角太多了",
+    image: "/images/3.png",
+    description: "繁華都市的拼圖挑戰",
+    placeholder: "/window.svg",
+  },
+  {
+    id: "4",
+    title: "孤獨搖滾",
+    image: "/images/2.png",
+    description: "令人垂涎的美食拼圖",
+    placeholder: "/globe.svg",
+  },
+]; 
